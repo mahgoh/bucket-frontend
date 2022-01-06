4b31c3b71046d1c9b24c9bd086d891e3bca4f8d1
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-01-06 16:23:49 +0100

    Allow apostrophes

--- static/utility.js
+++ static/utility.js
@@ -87,7 +87,7 @@ function setSubmit(identifier) {
 
 const regExpPresets = {
   default: '^[a-z0-9\\s]{MIN,MAX}$',
-  text: '^[a-z0-9äöüéèà.:,;!?()=$\\s_-]{MIN,MAX}$',
+  text: "^[a-z0-9äöüéèà.:,;'!?()=$\\s_-]{MIN,MAX}$",
   url: 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)',
 };
 
